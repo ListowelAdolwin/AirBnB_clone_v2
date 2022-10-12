f6050596d7ff98860b7a30d163093a7defff2e1f
mazi-kunle <olakunlemichael644@gmail.com>
2022-10-12 02:08:03 -0700

    created the schema for place

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
@@ -9,5 +10,7 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete, delete-orphan')
     # state_id = ""
     # name = ""
