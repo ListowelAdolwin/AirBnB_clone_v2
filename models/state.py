52d4339e9f37d0c72895a296b36f2c9889bc280c
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-12 16:14:08 +0000

    Added a condition to determine what relation type should be used

--- models/state.py
+++ models/state.py
@@ -5,24 +5,28 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship, backref
 import models
 from models.city import City
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
-                          cascade='all, delete, delete-orphan')
 
-    @property
-    def cities(self):
-        '''
-        getter attribute cities that returns the list of City
-        instances with state_id equals to the current State.id
-        '''
-        city_list = []
-        all_cities = models.storage.all(City)
-        for key in all_cities.keys():
-            if all_cities[key].state_id == self.id:
-                city_list.append(key)
-        return city_list
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+                            cascade='all, delete, delete-orphan')
+
+    else:
+        @property
+        def cities(self):
+            '''
+            getter attribute cities that returns the list of City
+            instances with state_id equals to the current State.id
+            '''
+            city_list = []
+            all_cities = models.storage.all(City)
+            for key in all_cities.keys():
+                if all_cities[key].state_id == self.id:
+                    city_list.append(key)
+            return city_list
