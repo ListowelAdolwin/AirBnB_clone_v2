ea861d99c6f979e538c3d7b56882a783e9ffcdcd
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-12 16:14:58 +0000

    Modified import statements

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,7 +4,14 @@ from sqlalchemy import create_engine
 import os
 from models.base_model import Base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models import *
+""" from models import *
+ """
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 valid_classes = ['City', 'State', 'User', 'Amenity', 'Place', 'Review']
 
